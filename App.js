c61a67477b26e7ce5037dbfb011b70447431e908
Andi Fahmi <andifahmi266@gmail.com>
2023-05-12 00:22:16 +0800

    Modified App

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+const PlaceholderImg = require('./assets/image/background1.jpeg')
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from 'react';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      console.log('You did not selected image');
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#25282e', alignItems: 'center' }}>
+      <View style={{ flex: 1, paddingTop: 58 }}>
+        <ImageViewer
+          PlaceholderImgSource={PlaceholderImg}
+          SelectedImage={selectedImage}
+        />
+        <Button theme={"primary"} label={'Chose a Picture'} onPress={pickImageAsync} />
+        <Button label={'Use This Photo'} />
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
