82b044158e75e589381737f8b6ce0d03a7f2b9a5
Andi Fahmi <andifahmi266@gmail.com>
2023-05-13 01:21:05 +0800

    Create EMojiList Component

---
+++ components/EmojiList.js
@@ -0,0 +1,49 @@
+import { View, Text, FlatList, Platform, StyleSheet, Pressable, Image } from 'react-native'
+import React, { useState } from 'react'
+
+const EmojiList = ({ onSelect, onCloseModal }) => {
+    const [emoji] = useState([
+        require('../assets/image/emo1.png'),
+        require('../assets/image/emo2.png'),
+        require('../assets/image/emo3.png'),
+        require('../assets/image/emo4.png'),
+        require('../assets/image/emo5.jpg'),
+        require('../assets/image/emo6.jpg'),
+    ])
+    return (
+        <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={Platform.OS === 'web' ? true : false}
+            data={emoji}
+            contentContainerStyle={styles.listContainer}
+            renderItem={({ item, index }) => {
+                return (
+                    <Pressable
+                        onPress={() => {
+                            onSelect(item)
+                            onCloseModal()
+                        }}>
+                        <Image source={item} key={index} style={styles.image} />
+                    </Pressable>
+                )
+            }}
+        />
+    )
+}
+
+export default EmojiList
+const styles = StyleSheet.create({
+    listContainer: {
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginRight: 20,
+    },
+});
